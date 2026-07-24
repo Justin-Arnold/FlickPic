55589a50bfb1234259f9bac5af6bd7a3bd492248
Justin Arnold <justin.arnold@programmer.net>
2026-07-24 13:25:50 -0400

    Initial Commit

---
+++ FlickPic/FlickPicApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickPicApp.swift
+//  FlickPic
+//
+//  Created by Justin Arnold on 7/24/26.
+//
+
+import SwiftUI
+
+@main
+struct FlickPicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlickPicTests/FlickPicTests.swift
@@ -0,0 +1,19 @@
+//
+//  FlickPicTests.swift
+//  FlickPicTests
+//
+//  Created by Justin Arnold on 7/24/26.
+//
+
+import Testing
+@testable import FlickPic
+
+struct FlickPicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
